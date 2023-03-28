1ee182e532d1077a3310c7ba672f6dd5a3582e6a
audrbar <102819309+audrbar@users.noreply.github.com>
2023-03-28 12:37:03 +0300

    basic app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="conatainer">
+      <header className="App-header">
+LAbas
+      </header>
+    </div>
+  );
+}
+
+export default App;
